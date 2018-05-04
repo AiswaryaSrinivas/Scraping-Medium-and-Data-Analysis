ca85f04413afc6913540e45b50d0a0350373715a
Aiswarya Srinivas <aiswaryar2010@gmail.com>
2018-05-05 00:31:10 +0530

    Update medium_scrapper_tag_archive.py

--- medium_scrapper_tag_archive.py
+++ medium_scrapper_tag_archive.py
@@ -4,7 +4,7 @@ Created on Tue May  1 23:30:54 2018
 
 @author: Aiswarya
 
-This scrapper extracts data for a given date and a given tag
+This scrapper extracts data for a given date range for a particular medium Tag
 """
 
 import scrapy
@@ -28,35 +28,10 @@ class MediumPost(scrapy.Spider):
         start_urls = ['https://medium.com/tag/'+self.tagSlug.strip("'")+'/archive/']
         print(start_urls)
         
-        cookie={
-                                '__cfduid':'da03ee0d47b0f9fc62cd620eb2e19740d1525156135',
-                                '_ga':'GA1.2.1177081718.1525156137',
-                                '_gid':'GA1.2.1879325541.1525156137',
-                                'lightstep_guid/medium-web':'288180d9401c5407',
-                                'lightstep_session_id':'fc3f8a3ecff727b0',
-                                'pr':'1.25',
-                                'tz':'-330', 
-                                'uid':'7bbdad3b3571',
-                                'sid':'1:BebZ9kr91ptfgtxqzANJqwhq8CuEq1XMkZT1fE3Wm23QnIknPhEjnUEItoNqAm7T',
-                                'xsrf':'hBL2qH8I5ckb',
-                                'xsrf':'kir9Fl-1QBDt7ZZT', 
-                                '__cfruid':'7d89b52dc669786052af0fed2fa8785ca30e374f-1525197176',
-                                'sz':'674'
-                                  }
-        header = {
-                        'accept': 'application/json',
-                        'accept-encoding': 'gzip, deflate, br',
-                        'accept-language': 'en-US,en;q=0.9,ta;q=0.8',
-                        'content-type': 'application/json',
-                        'referer': 'https://medium.com/tag/data-science/archive/2018/03/23',
-                        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
-                        'x-client-date': '1525197603242',
-                        'x-obvious-cid': 'web',
-                        'x-opentracing': '{"ot-tracer-spanid":"1cbe927d0f29e9","ot-tracer-traceid":"8f0a5b5308eab","ot-tracer-sampled":"true"}',
-                        'x-xsrf-token': 'hBL2qH8I5ckb'
-        }   
-        #start_date=self.start_date
-        #end_date=self.end_date
+        #Header and cookie information can be got from the Network Tab in Developer Tools
+        cookie=cookie
+        header =header 
+       
         startDate=datetime.strptime(self.start_date,"%Y%m%d")
         endDate=datetime.strptime(self.end_date,"%Y%m%d")
         delta=endDate-startDate
